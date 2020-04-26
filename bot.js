20ddb82c86c175d26886bd735f39a257da231535
metanoia <metanoia3902@gmail.com>
2020-04-26 22:44:07 +0100

    Improved leaderboard command, added give multiple oranges command, bug fixes.

--- bot.js
+++ bot.js
@@ -18,25 +18,29 @@ global.client.once('ready', () => {
 
 	global.rinchanSQL.init();
 
+	modules.orange.setIcon();
+
 	console.log('Ready!');
 });
 
-global.client.on('message', message => {
+global.client.on('message', (message) => {
 	console.log(message.content);
 
-	if (message.isMentioned(global.client.user) && message.guild) {
-		var command = message.content.substring(message.content.search('>') + 2);
+	rinTest = new RegExp(/^<@601807905053736991>|^<@!601807905053736991>/);
 
-		command = command.replace('?', '');
-		command = command.replace('!', '');
+	if (message.mentions.has(global.client.user) && message.guild && rinTest.test(message.content)) {
+		var command = message.content.replace(/^<@![0-9]*>\s*|^<@[0-9]*>\s*/, '');
+		command = command.replace(/\s\s+/g, ' ');
+
+		console.log(command);
 
 		for (var k in modules) {
 			if (modules.hasOwnProperty(k)) {
 				for (var c in modules[k].config.cmd) {
 					for (var v = 0; v < modules[k].config.cmd[c].length; v++) {
 						let cmdRegex = new RegExp(modules[k].config.cmd[c][v], 'i');
-						if (command.search(cmdRegex) >= 0) {
-							modules[k][c](message);
+						if (cmdRegex.test(command)) {
+							modules[k][c](message, command);
 							return;
 						}
 					}
@@ -66,7 +70,7 @@ global.client.on('message', message => {
 
 function addModules() {
 	console.log('Adding modules...');
-	config.modules.forEach(function(value) {
+	config.modules.forEach(function (value) {
 		modules[value] = require(`./rinchan_modules/${value}/module.js`);
 
 		if (typeof modules[value].init == 'function') {
@@ -77,20 +81,19 @@ function addModules() {
 	});
 }
 
-global.client.on('guildMemberAdd', member => {
-	const channel = member.guild.channels.find(ch => ch.name === 'lounge');
+global.client.on('guildMemberAdd', (member) => {
+	const channel = member.guild.channels.cache.find((ch) => ch.name === 'lounge');
 
 	if (!channel) return;
 
-	channel.send(`Welcome to my server, ${member}`);
-	channel.send({
+	channel.send(`Welcome to my server, ${member}`, {
 		files: [
 			'https://cdn.discordapp.com/attachments/601856655873015831/601856714135830538/53954b9f34fb92120d87dd65ceca7815.gif',
 		],
 	});
 });
 
-global.client.on('guildMemberRemove', member => {});
+global.client.on('guildMemberRemove', (member) => {});
 
 /*
 function checkAdminCommands(message) {
@@ -106,6 +109,10 @@ function checkAdminCommands(message) {
 
 };*/
 
-setInterval(function() {
+setInterval(function () {
 	global.rinchanSQL.setTries.run();
 }, 14400000);
+
+global.client.on('exit', (exitCode) => {
+	global.rinchanSQL.close();
+});
--- rinchan_modules/orange/module.js
+++ rinchan_modules/orange/module.js
@@ -16,8 +16,20 @@ var orange = {
 		return false;
 	},
 
-	giveOrange(message) {
+	giveOrange(message, command) {
 		let usersArray = message.mentions.users.array();
+
+		var testRegex = new RegExp(/<@![0-9]+>|<@[0-9]+>/);
+
+		let testID = command.match(testRegex);
+		if (testID) {
+			testID = String(testID).substr(3, 18);
+		}
+
+		if (message.guild.member(testID) == null && testRegex.test(command)) {
+			message.channel.send('They arent in the server <:rinconfuse:687276500998815813>');
+			return;
+		}
 
 		if (usersArray.length == 1 && usersArray[0].id == global.client.user.id) {
 			orange.feedOrange(message);
@@ -33,23 +45,52 @@ var orange = {
 			message.channel.send('You cant give oranges to yourself!');
 			return;
 		}
+		let test = new RegExp('give [0-9]+ oranges to');
+		if (message.content.match(test)) {
+			let num = command.match(/\d+/)[0];
 
-		let sourceUser = global.rinchanSQL.getUser(message.author.id, message.guild.id);
-		let destUser = global.rinchanSQL.getUser(usersArray[destId].id, message.guild.id);
+			if (num > 0) {
+				let sourceUser = global.rinchanSQL.getUser(message.author.id, message.guild.id);
+				let destUser = global.rinchanSQL.getUser(usersArray[destId].id, message.guild.id);
 
-		if (sourceUser.oranges < 1) {
-			message.channel.send('You dont have an orange to give');
+				if (sourceUser.oranges < num) {
+					message.channel.send('You dont have ' + num + ' oranges to give');
+				} else {
+					sourceUser.oranges -= parseInt(num);
+					destUser.oranges += parseInt(num);
+
+					message.channel.send('Ok, you gave ' + num + ' oranges');
+				}
+
+				global.rinchanSQL.setOrange.run(sourceUser);
+				global.rinchanSQL.setOrange.run(destUser);
+				return;
+			} else {
+				message.channel.send(
+					'Fine, no oranges for ' +
+						message.mentions.users.get(usersArray[destId].id).username +
+						' <:smugrin:674044431502016532>'
+				);
+				return;
+			}
 		} else {
-			sourceUser.oranges--;
-			destUser.oranges++;
-			message.channel.send('Ok, you gave an orange');
-		}
+			let sourceUser = global.rinchanSQL.getUser(message.author.id, message.guild.id);
+			let destUser = global.rinchanSQL.getUser(usersArray[destId].id, message.guild.id);
 
-		global.rinchanSQL.setOrange.run(sourceUser);
-		global.rinchanSQL.setOrange.run(destUser);
+			if (sourceUser.oranges < 1) {
+				message.channel.send('You dont have an orange to give');
+			} else {
+				sourceUser.oranges--;
+				destUser.oranges++;
+				message.channel.send('Ok, you gave an orange');
+			}
+
+			global.rinchanSQL.setOrange.run(sourceUser);
+			global.rinchanSQL.setOrange.run(destUser);
+		}
 	},
 
-	leaderboard(message) {
+	async leaderboard(message) {
 		var board = global.rinchanSQL.getBoard.all();
 
 		var output = '';
@@ -58,18 +99,28 @@ var orange = {
 			return b.oranges - a.oranges;
 		});
 
-		board.forEach(async function(user, index) {
+		await board.forEach(async function (user, index) {
 			if (user.oranges > 0) {
 				try {
-					let usr = await global.client.fetchUser(user.user);
-					let output =
-						index + 1 + '. ' + usr['username'] + ' has ' + user.oranges + (user.oranges > 1 ? ' oranges' : ' orange');
-					message.channel.send(output);
+					let usr = await message.guild.members.fetch(user.user);
+					output +=
+						index +
+						1 +
+						'. ' +
+						usr.displayName +
+						' has ' +
+						user.oranges +
+						(user.oranges > 1 ? ' oranges' : ' orange') +
+						'\n';
 				} catch (e) {
 					console.log(e);
 				}
 			}
 		});
+
+		output = output.replace(/\s+$/, '');
+
+		message.channel.send(output);
 	},
 
 	checkForEmote(message) {
@@ -119,12 +170,14 @@ var orange = {
 					user.oranges--;
 					user.affection++;
 					orange.hunger--;
+					orange.setIcon();
 					break;
 				case 2:
 					message.channel.send(`I'm starving! What took you so long`);
 					user.oranges--;
 					user.affection++;
 					orange.hunger--;
+					orange.setIcon();
 					break;
 			}
 		}
@@ -135,12 +188,20 @@ var orange = {
 	harvestOrange(message) {
 		let user = global.rinchanSQL.getUser(message.author.id, message.guild.id);
 
+		let chance = Math.random();
+		console.log(chance);
+
 		if (user.tries > 0) {
-			if (Math.random() > 0.5) {
+			if (0 < chance && chance < 0.05) {
+				this.easterEgg(message);
+				console.log('1');
+			} else if (0.05 < chance && chance < 0.6) {
 				user.oranges++;
-				message.channel.send('Found an Orange!');
-			} else {
-				message.channel.send('Couldnt find anything');
+				this.foundOrange(message);
+				console.log('2');
+			} else if (0.6 < chance && chance < 1) {
+				this.couldntFind(message);
+				console.log('3');
 			}
 
 			user.tries--;
@@ -149,6 +210,22 @@ var orange = {
 		}
 
 		global.rinchanSQL.setOrange.run(user);
+	},
+
+	easterEgg(message) {
+		message.channel.send('Found a Len! <:rinwao:701505851449671871>', {
+			files: [
+				'https://cdn.discordapp.com/attachments/601856655873015831/703286810133921892/b5396d6cadd36f32424c4bb1b48913d30b7deb86.jpg',
+			],
+		});
+	},
+
+	foundOrange(message) {
+		message.channel.send('Found an Orange!');
+	},
+
+	couldntFind(message) {
+		message.channel.send('Couldnt find anything... <:rinyabai:635101260080480256>');
 	},
 
 	catchOrange(message) {
@@ -164,7 +241,7 @@ var orange = {
 		message.channel.send(''); //ask to pick
 	},
 
-	catchChoice: function(message) {
+	catchChoice: function (message) {
 		var pattern = [
 			Math.round(Math.random()),
 			Math.round(Math.random()),
@@ -172,19 +249,19 @@ var orange = {
 			Math.round(Math.random()),
 		];
 
-		setTimeout(function() {
+		setTimeout(function () {
 			catchMessage.edit('');
 		}, 1000);
 
-		setTimeout(function() {
+		setTimeout(function () {
 			catchMessage.edit('');
 		}, 1000);
 
-		setTimeout(function() {
+		setTimeout(function () {
 			catchMessage.edit('');
 		}, 1000);
 
-		setTimeout(function() {
+		setTimeout(function () {
 			catchMessage.edit('');
 		}, 1000); //Final
 
@@ -204,7 +281,7 @@ var orange = {
 		catching = false;
 	},
 
-	hungry: function(message) {
+	hungry: function (message) {
 		switch (orange.hunger) {
 			case 0: {
 				message.channel.send('Im stuffed <:rinchill:600745019514814494>');
@@ -221,12 +298,38 @@ var orange = {
 		}
 		return;
 	},
+
+	setIcon() {
+		switch (orange.hunger) {
+			case 0: {
+				global.client.guilds.cache
+					.get('585071519638487041')
+					.setIcon('https://cdn.discordapp.com/attachments/601856655873015831/693153043742457977/trubosakura.gif');
+				return;
+			}
+			case 1: {
+				global.client.guilds.cache
+					.get('585071519638487041')
+					.setIcon('https://cdn.discordapp.com/attachments/601856655873015831/693153034519183370/sakuraflap.gif');
+				return;
+			}
+			case 2: {
+				global.client.guilds.cache
+					.get('585071519638487041')
+					.setIcon('https://cdn.discordapp.com/attachments/601856655873015831/693159742608113784/sakura.png');
+				return;
+			}
+
+			/* .find(guild => guild.id === '585071519638487041') */
+		}
+	},
 };
 
-setInterval(function() {
+setInterval(function () {
 	if (orange.hunger < orange.config.maxHunger) {
 		orange.hunger++;
+		orange.setIcon();
 	}
-}, 57600000);
+}, 14400000);
 
 module.exports = orange;
