ffc1f2328d865e1a952f10a405e4a41f20b05b28
metanoia <metanoia3902@gmail.com>
2020-05-29 00:11:00 +0100

    Give oranges bugfix

--- rinchan_modules/orange.js
+++ rinchan_modules/orange.js
@@ -5,6 +5,15 @@ module.exports = {
 	hunger: 2,
 	maxHunger: 5,
 
+	hungerIcon: {
+		0: 'https://cdn.discordapp.com/emojis/697594415790686218.png', //Stuffed
+		1: 'https://cdn.discordapp.com/emojis/620970346626940958.gif', //Turbo Flap
+		2: 'https://cdn.discordapp.com/emojis/591970280843247616.gif', //Normal Flap
+		3: 'https://cdn.discordapp.com/emojis/243272900449271808.png', //NoFlap
+		4: 'https://cdn.discordapp.com/emojis/628298482616107018.png', //Pout
+		5: 'https://cdn.discordapp.com/emojis/620576239224225835.png', //Angrey
+	},
+
 	init() {
 		let Reaction = require('../reactions/reaction.js');
 
@@ -32,7 +41,7 @@ module.exports = {
 
 	giveNumOranges(message, command, num) {
 		let sourceUser = global.rinchanSQL.getUser(message.author.id, message.guild.id);
-		let sourceInventory = global.rinchanSQL.getInventory(sourceUser, orangeObj);
+		let sourceInventory = global.rinchanSQL.getInventory(sourceUser, this.orangeObj);
 
 		let orangeString = num > 1 ? num + ' oranges' : 'an orange';
 
