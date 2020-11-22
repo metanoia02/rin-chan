abf546593ad179b7f53c71b1f50fd2344e15c170
metanoia <metanoia3902@gmail.com>
2020-11-22 19:30:35 +0000

    Steal command rebalanced and new reactions added

--- commands/steal.js
+++ commands/steal.js
@@ -3,33 +3,46 @@ const commandUtils = require('../utils/commandUtils.js');
 const utils = require('../utils/utils.js');
 const Discord = require('discord.js');
 const CommandException = require('../utils/CommandException.js');
+const rinChan = require('../rinChan/rinChan.js');
+const Reaction = require('../reactions/reaction');
 
 module.exports = {
   config: {
-    training: [{locale: 'en', string: 'steal %object% from %tag%'}],
+    training: [{locale: 'en', string: 'steal %Entity% from %tag%'}],
 
-    intent: 'stealObject',
+    intent: 'steal',
     commandName: 'Steal',
     description: 'Try and make an orange heist. Use the targets username and tag e.g. username#1234',
 
     scope: 'channel',
 
-    stealableObjects: [
-      {objectName: 'orange', func: 'stealOranges'},
-      {objectName: 'Len', func: 'stealLens'},
+    stealableEntitys: [
+      {entityId: 'orange', func: 'stealOranges'},
+      {entityId: 'kagamineLen', func: 'stealLens'},
     ],
-    orangeStealCooldown: 86400000,
+    orangeStealCooldown: 1,
+  },
+
+  init() {
+    this.passStealReact = new Reaction('../reactions/steal/passSteal.json', this.config.commandName);
+    this.failStealReact = new Reaction('../reactions/steal/failSteal.json', this.config.commandName);
+    this.notAgainReact = new Reaction('../reactions/steal/notAgain.json', this.config.commandName);
   },
 
   async run(message, args) {
     commandUtils.validateSingleTagAction(args);
-    commandUtils.validateSingleObjectAction(args);
+
+    if (args.tradable.length === 0) {
+      throw new CommandException(`You didn't tell me what`, `rinwha.png`);
+    } else if (args.tradable.length !== 1) {
+      throw new CommandException(`Which one?`, `rinconfuse.png`);
+    }
 
     const sourceUser = new User(message);
     const stealUser = args.tags[0];
-    const object = args.objects[0];
+    const entity = args.entities[0];
 
-    const functionName = this.config.stealableObjects.find((ele) => ele.objectName === object.getName()).func;
+    const functionName = this.config.stealableEntitys.find((ele) => ele.entityId === entity.id).func;
     this[functionName](message, sourceUser, stealUser);
   },
 
@@ -45,31 +58,34 @@ module.exports = {
     } else if (stealUser.getId() == message.author.id) {
       throw new CommandException(`Don't temp me!`, 'creeprin.png');
     } else if (now.getTime() - sourceUser.getLastSteal() > this.config.orangeStealCooldown) {
-      if (stealUser.getObjectQuantity('orange') < 5) {
+      if (stealUser.getEntityQuantity('orange') < 10) {
         throw new CommandException(`They aren't a good target.`, 'rinshrug.png');
       } else if (sourceUser.getAffection() > stealUser.getAffection()) {
         const chance = Math.floor(Math.random() * 100) + 1;
         if (chance >= 90) {
-          sourceUser.getAffection() > 9 ? sourceUser.changeAffection(-10) : sourceUser.setAffection(0);
+          sourceUser.changeAffection(20);
+          rinChan.changeMood(1);
 
-          const stolenOranges =
-            stealUser.getObjectQuantity('orange') / 10 > 10 ?
-              10 :
-              Math.round(stealUser.getObjectQuantity('orange') / 10);
+          const stolenOranges = Math.min(10, Math.round(stealUser.getEntityQuantity('orange') / 10));
 
-          sourceUser.changeObjectQuantity('orange', stolenOranges);
-          stealUser.changeObjectQuantity('orange', -stolenOranges);
+          sourceUser.changeEntityQuantity('orange', stolenOranges);
+          stealUser.changeEntityQuantity('orange', -stolenOranges);
           sourceUser.setLastSteal();
 
-          message.channel.send(
-            `I did it! Here's ` +
-              stolenOranges +
-              (stolenOranges === 1 ? ' orange' : ' oranges') +
-              ' from ' +
-              stealUser.getNickname()
-          );
+          const orangePlural = stolenOranges === 1 ? 'orange' : 'oranges';
+          const stealString = `${stolenOranges} ${orangePlural}`;
+
+          const passedStealEmbed = this.passStealReact.getEmbed(sourceUser)
+            .addField('You got:', stealString, true);
+
+          message.channel.send(passedStealEmbed);
         } else {
-          message.channel.send('I got caught... <:rinbuaaa:686741811850510411>');
+          const failStealEmbed = this.failStealReact.getEmbed(sourceUser);
+
+          sourceUser.changeAffection(-40);
+          rinChan.changeMood(-1);
+
+          message.channel.send(failStealEmbed);
         }
         sourceUser.setLastSteal();
       } else {
@@ -78,11 +94,7 @@ module.exports = {
     } else {
       const duration = utils.getCooldown(this.config.orangeStealCooldown, sourceUser.getLastSteal());
 
-      const notAgainEmbed = new Discord.MessageEmbed()
-        .setColor('#FF0000')
-        .setTitle('Steal Oranges')
-        .setDescription('Not again')
-        .setThumbnail('https://cdn.discordapp.com/emojis/635101260080480256.png')
+      const notAgainEmbed = this.notAgainReact.getEmbed(sourceUser)
         .addField('You can try again in:', duration, true);
 
       message.channel.send(notAgainEmbed);
