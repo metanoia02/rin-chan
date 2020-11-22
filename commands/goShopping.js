50fde1a07d1dc3941618854b2e5cba2945e0effe
metanoia <metanoia3902@gmail.com>
2020-11-22 22:02:22 +0000

    Shop updated with new entity system and html template

--- commands/goShopping.js
+++ commands/goShopping.js
@@ -1,11 +1,13 @@
 const CommandException = require('../utils/CommandException.js');
-const nodeHtmlToImage = require('node-html-to-image');
 const Discord = require('discord.js');
 const database = require('../utils/sql.js');
 const utils = require('../utils/utils.js');
 const rinChan = require('../rinChan/rinChan.js');
 const User = require('../utils/User.js');
 const entityManager = require('../utils/entityManager');
+const fs = require('fs');
+const handlebars = require('handlebars');
+const puppeteer = require('puppeteer');
 
 module.exports = {
   config: {
@@ -35,33 +37,33 @@ module.exports = {
       return response.author.id === message.author.id;
     };
 
-    this.sendShopEmbed(message).then(() => {
-      message.author.createDM().then((dmChannel) => {
-        const collector = dmChannel.createMessageCollector(filter, {time: 60000});
-        rinChan.setCollecting(true);
+    await this.sendShopEmbed(message);
 
-        collector.on('collect', (collected) => {
-          try {
-            const buyRegex = new RegExp(this.config.phrases.buy);
-            const sellRegex = new RegExp(this.config.phrases.sell);
-            const leaveRegex = new RegExp(this.config.phrases.leave);
+    message.author.createDM().then((dmChannel) => {
+      const collector = dmChannel.createMessageCollector(filter, {time: 60000});
+      rinChan.setCollecting(true);
 
-            if (buyRegex.test(collected.content)) {
-              this.buyEntity(collected.content, message);
-            } else if (sellRegex.test(collected.content)) {
-              this.sellEntity(collected.content, message);
-            } else if (leaveRegex.test(collected.content)) {
-              collector.stop();
-              rinChan.setCollecting(false);
-            }
-          } catch (err) {
-            utils.handleError(err, this.config.commandName, message.author);
+      collector.on('collect', (collected) => {
+        try {
+          const buyRegex = new RegExp(this.config.phrases.buy);
+          const sellRegex = new RegExp(this.config.phrases.sell);
+          const leaveRegex = new RegExp(this.config.phrases.leave);
+
+          if (buyRegex.test(collected.content)) {
+            this.buyEntity(collected.content, message);
+          } else if (sellRegex.test(collected.content)) {
+            this.sellEntity(collected.content, message);
+          } else if (leaveRegex.test(collected.content)) {
+            collector.stop();
+            rinChan.setCollecting(false);
           }
-        });
+        } catch (err) {
+          utils.handleError(err, this.config.commandName, message.author);
+        }
+      });
 
-        collector.on('end', (collected) => {
-          message.author.send(`I'm bored lets go`);
-        });
+      collector.on('end', (collected) => {
+        message.author.send(`I'm bored lets go`);
       });
     });
   },
@@ -71,20 +73,20 @@ module.exports = {
 
     const matchesArray = [...messageContent.matchAll(reg)][0];
 
-    const Entity = entityManager.get(matchesArray[2]);
+    const entity = entityManager.find(matchesArray[2]);
     const quantity = matchesArray[1];
 
-    return {Entity: Entity, quantity: quantity};
+    return {entity: entity, quantity: quantity};
   },
 
   buyEntity(collectedContent, message) {
     const order = this.getEntityQuantity(collectedContent, this.config.phrases.buy);
     const user = new User(message);
-    const price = order.quantity * order.Entity.value;
+    const price = order.quantity * order.entity.value;
 
-    const EntityString = order.quantity > 1 ? order.Entity.plural : order.Entity.name;
+    const entityString = order.quantity > 1 ? order.entity.plural : order.entity.name;
 
-    if (order.Entity.id === 'orange') {
+    if (order.entity.id === 'orange') {
       throw new CommandException(`What's the point of that?`, 'rinconfuse.png');
     }
 
@@ -92,12 +94,12 @@ module.exports = {
       throw new CommandException(`You don't have enough oranges for that.`, 'rinwha.png');
     }
 
-    this.shopChangeEntity(order.Entity, -order.quantity);
+    this.shopChangeEntity(order.entity, -order.quantity);
 
-    user.changeEntityQuantity(order.Entity.id, parseInt(order.quantity));
+    user.changeEntityQuantity(order.entity.id, parseInt(order.quantity));
     user.changeEntityQuantity('orange', -parseInt(price));
 
-    message.author.send('Ok you bought ' + order.quantity + ' ' + EntityString);
+    message.author.send('Ok you bought ' + order.quantity + ' ' + entityString);
     this.sendShopEmbed(message);
   },
 
@@ -105,113 +107,82 @@ module.exports = {
     const order = this.getEntityQuantity(collectedContent, this.config.phrases.sell);
 
     const user = new User(message);
-    const price = order.quantity * order.Entity.value * 0.5;
+    const price = order.quantity * order.entity.value * 0.5;
 
-    const EntityString = order.quantity > 1 ? order.Entity.plural : order.Entity.name;
+    const entityString = order.quantity > 1 ? order.entity.plural : order.entity.name;
 
-    if (user.getEntityQuantity(order.Entity.id) < order.quantity) {
-      throw new CommandException(`You don't have ${order.quantity} ${EntityString}.`, 'rinwha.png');
+    if (user.getEntityQuantity(order.entity.id) < order.quantity) {
+      throw new CommandException(`You don't have ${order.quantity} ${entity}.`, 'rinwha.png');
     }
 
-    if (order.Entity.id === 'orange') {
+    if (order.entity.id === 'orange') {
       throw new CommandException(`What's the point of that?`, 'rinconfuse.png');
     }
 
-    this.shopChangeEntity(order.Entity, order.quantity);
+    this.shopChangeEntity(order.entity, order.quantity);
 
     user.changeEntityQuantity('orange', parseInt(price));
-    user.changeEntityQuantity(order.Entity.id, -parseInt(order.quantity));
+    user.changeEntityQuantity(order.entity.id, -parseInt(order.quantity));
 
-    message.author.send('Ok you sold ' + order.quantity + ' ' + EntityString);
+    message.author.send('Ok you sold ' + order.quantity + ' ' + entityString);
     this.sendShopEmbed(message);
   },
 
-  shopChangeEntity(Entity, modifier) {
-    const shop = database.getShopStock(Entity.id);
+  shopChangeEntity(entity, modifier) {
+    const shop = database.getShopStock(entity.id);
     const shopOranges = database.getShopStock('orange');
 
     if (shop.quantity < modifier * -1 && modifier < 0) {
       throw new CommandException(`There's not enough stock`, 'rinwha.png');
     }
-    if (modifier > 0 && shopOranges.quantity < modifier * Entity.value) {
+    if (modifier > 0 && shopOranges.quantity < modifier * entity.value) {
       throw new CommandException(`The shop doesn't have enough oranges to make the purchase.`, 'rinwha.png');
     }
 
     shop.quantity += parseInt(modifier);
-    shopOranges.quantity += parseInt(Entity.value * (modifier * -1));
+    shopOranges.quantity += parseInt(entity.value * (modifier * -1));
 
     database.setShopStock.run(shopOranges);
     database.setShopStock.run(shop);
   },
-  restock(message, command, cmdRegex) {},
-
-  makeSale() {},
 
   async sendShopEmbed(message) {
-    let embedString = `
-      <html>
-      <head>
-          <style>
-          body{
-              background-color: #2f3136;
-              color: #d9dadb;
-              width:400px;
-              font-family: Whitney,Helvetica Neue,Helvetica,Arial,sans-serif;
-              font-size:20px;
-          }
-          table {
-              width:100%;
-              padding:5px;
-              text-align:center;
-          }
-          th {
-              font-size:24px;
-          }
-          </style>
-      </head>
-      <body>
-      <table>
-      <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Stock</th>
-      </tr>`;
+    const stock = database.getAllShopStock();
 
-    const stock = database.getShopStock();
+    const shopStock = stock.reduce((acc, ele) => {
+      acc.push({name: ele.name, value: entityManager.get(ele.id).value, quantity: ele.quantity});
+      return acc;
+    }, []);
 
-    stock.forEach((ele) => {
-      embedString += `
-          <tr>
-              <td>${utils.capitalizeFirstLetter(ele.name)}</td>
-              <td>${entityManager.get(ele.name).value}</td>
-              <td>${ele.quantity}</td>
-          </tr>`;
-    }); 
+    const htmlFile = fs.readFileSync('./commands/shop.html', 'utf8');
+    const template = handlebars.compile(htmlFile);
+    const result = template({stock: shopStock});
 
-    embedString += `
-      </table>
-      </body>
-      </html>`;
-
-    nodeHtmlToImage({
-      output: './images/embeds/shop.png',
-      html: embedString,
-      transparent: false,
-    }).then(() => {
-      const attachment = new Discord.MessageAttachment('./images/embeds/shop.png', 'shop.png');
-      const thumbnail = new Discord.MessageAttachment('./images/shop/thumbnail.jpg', 'thumbnail.jpg');
-
-      const embed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Orange Kingdom General Store')
-        .setDescription('The best value every day for discerning Rin-chans')
-        .setThumbnail('attachment://thumbnail.jpg')
-        .attachFiles(attachment)
-        .attachFiles(thumbnail)
-        .setImage('attachment://shop.png')
-        .setFooter(`Say buy or sell <quantity> <item name> eg. buy 2 lens. To leave say finished. This shop buys back items for 50% of their value.`);
-
-      message.author.send(embed);
+    //make screenshot
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 400,
+      height: 600
     });
+    await page.setContent( result );
+    const inventoryImage = await page.screenshot();
+
+    //discord
+    const thumbnail = new Discord.MessageAttachment('./images/shop/thumbnail.jpg', 'thumbnail.jpg');
+    const attachment = new Discord.MessageAttachment(inventoryImage, 'shop.png');
+    const shopEmbed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Orange Kingdom General Store')
+      .setDescription('The best value every day for discerning Rin-chans')
+      .setThumbnail('attachment://thumbnail.jpg')
+      .attachFiles(attachment)
+      .attachFiles(thumbnail)
+      .setImage('attachment://shop.png')
+      .setFooter(`Say buy or sell <quantity> <item name> eg. buy 2 lens. To leave say finished. This shop buys back items for 50% of their value.`);
+
+    message.author.send(shopEmbed);
+
+    await browser.close();
   },
 };
