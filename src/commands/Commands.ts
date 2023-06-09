74f618fe55e160f938695e6a0b72c1b55322f622
metanoia02 <metanoia3902@gmail.com>
2023-06-09 20:37:55 +0100

    Corrected command list types

--- src/commands/Commands.ts
+++ src/commands/Commands.ts
@@ -1,4 +1,4 @@
 import { Command } from "../interfaces/Command";
 import { Collection } from "discord.js";
 
-export const CommandList: Collection<any, Command> = new Collection();
+export const CommandList: Collection<String, Command> = new Collection();
