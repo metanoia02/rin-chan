dd032283d9ccd2b41eba518f06ad97f9d16891af
metanoia02 <metanoia3902@gmail.com>
2023-06-09 20:39:35 +0100

    Added autocomplete handling

--- src/listeners/interactionCreate.ts
+++ src/listeners/interactionCreate.ts
@@ -2,38 +2,48 @@ import { Client, Events, Interaction } from "discord.js";
 import { CommandList } from "../commands/Commands";
 
 export default (client: Client): void => {
-    client.on(Events.InteractionCreate, async (interaction:Interaction) => {
-        if (interaction.isChatInputCommand()) {
-            const command = CommandList.get(interaction.commandName);
-    
-            if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
-        
-            try {
-                await command.execute(interaction);
-            } catch (error) {
-                console.error(error);
-                if (interaction.replied || interaction.deferred) {
-                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-                } else {
-                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-                }
-            }
-        } else if (interaction.isAutocomplete()) {
-            const command = CommandList.get(interaction.commandName);
+  client.on(Events.InteractionCreate, async (interaction: Interaction) => {
+    if (interaction.isChatInputCommand()) {
+      const command = CommandList.get(interaction.commandName);
 
-            if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
-    
-            try {
-                await command.autocomplete!(interaction);
-            } catch (error) {
-                console.error(error);
-            }
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
+
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+          await interaction.followUp({
+            content: "There was an error while executing this command!",
+            ephemeral: true,
+          });
+        } else {
+          await interaction.reply({
+            content: "There was an error while executing this command!",
+            ephemeral: true,
+          });
         }
-    });
-}
+      }
+    } else if (interaction.isAutocomplete()) {
+      const command = CommandList.get(interaction.commandName);
+
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
+
+      try {
+        await command.autocomplete!(interaction);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  });
+};
