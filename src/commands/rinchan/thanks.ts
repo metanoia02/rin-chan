9ebb447f33809fdbee1b176668598fa07d975b47
metanoia02 <metanoia3902@gmail.com>
2023-08-24 22:24:37 +0100

    Added thanks command

---
+++ src/commands/rinchan/thanks.ts
@@ -0,0 +1,16 @@
+import { ICommand } from '../../interfaces/ICommand';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { ChatInputCommandInteraction } from 'discord.js';
+import { thanks as thanksReact } from '../../reactions/misc/thanks';
+import { ReactionMaker } from 'src/reactions/ReactionMaker';
+import { User } from 'src/entity/User';
+
+export const thanks: ICommand = {
+  data: new SlashCommandBuilder().setName('thanks').setDescription('Thank Rin-chan.'),
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    const user = await User.get(interaction.user.id, interaction.guildId!);
+
+    interaction.reply(await ReactionMaker.getEmbed(thanksReact, user));
+  },
+};
