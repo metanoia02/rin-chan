5e28a6bd028f46d4c848b9294e428f6b96603872
metanoia02 <metanoia3902@gmail.com>
2023-08-24 22:01:51 +0100

    Added hungry command

---
+++ src/commands/rinchan/hungry.ts
@@ -0,0 +1,18 @@
+import { ICommand } from '../../interfaces/ICommand';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { ChatInputCommandInteraction } from 'discord.js';
+import { hungry as hungryReact } from '../../reactions/rinchan/hungry';
+import { ReactionMaker } from 'src/reactions/ReactionMaker';
+import { User } from 'src/entity/User';
+
+export const hungry: ICommand = {
+  data: new SlashCommandBuilder()
+    .setName('hungry')
+    .setDescription('Ask Rin-chan if she is hungry.'),
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    const user = await User.get(interaction.user.id, interaction.guildId!);
+
+    interaction.reply(await ReactionMaker.getEmbed(hungryReact, user));
+  },
+};
