d08394f049c6c114d76b55bfa8c8206295a62465
metanoia02 <metanoia3902@gmail.com>
2023-09-04 16:22:48 +0100

    Added sleep command

---
+++ src/commands/admin/sleep.ts
@@ -0,0 +1,29 @@
+import { ICommand } from '../../interfaces/ICommand';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { ChatInputCommandInteraction, PermissionFlagsBits, TextBasedChannel } from 'discord.js';
+import { Forever } from 'forever';
+import { client } from 'src/client';
+import { Server } from 'src/entity/Server';
+import { commandEmbed } from 'src/util/commands';
+import { CommandFailedEvent } from 'typeorm';
+
+export const sleep: ICommand = {
+  data: new SlashCommandBuilder()
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .setName('sleep')
+    .setDescription('Go into maintenance mode.'),
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    Forever.startServer();
+
+    const server = await Server.get(interaction.guildId!);
+    if (server.botChannel) {
+      const botChannel = client.guilds.cache.get(server.id)?.channels.cache.get(server.botChannel);
+      if (botChannel?.isTextBased) {
+        (botChannel as TextBasedChannel).send(commandEmbed(`I'll be right back!`, 'rinbath.png'));
+      }
+    }
+
+    await Forever.stopAll();
+  },
+};
