73ac553506103b00f6bd848d78130d49c596bc92
metanoia02 <metanoia3902@gmail.com>
2023-09-19 00:51:01 +0100

    Refactored inventory

--- src/commands/items/inventory.ts
+++ src/commands/items/inventory.ts
@@ -1,43 +1,19 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-const handlebars = require('handlebars');
-const puppeteer = require('puppeteer');
-
 import { ICommand } from '../../interfaces/ICommand';
-
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { AttachmentBuilder, ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
+import { ChatInputCommandInteraction } from 'discord.js';
 import { User } from '../../entity/User';
-import { generateImage } from '../../util/generateImage';
-import { InventoryContent, InventoryRow } from '../../types/InventoryContent';
+import { InventoryStack } from '../../entity/InventoryStack';
+import { getInventory } from '../../util/getInventory';
+
+function filterInventory(inventoryItem: InventoryStack): boolean {
+  return inventoryItem.quantity > 0 && !inventoryItem.item.videoUrl;
+}
 
 export const inventory: ICommand = {
   data: new SlashCommandBuilder().setName('inventory').setDescription('Show your inventory.'),
   async execute(interaction: ChatInputCommandInteraction) {
     const user = await User.get(interaction.user.id, interaction.guildId!);
 
-    const content: InventoryContent = { inventorySlots: [] };
-
-    for (const inventoryItem of user.inventory) {
-      if (inventoryItem.quantity > 0) {
-        const itemImage = fs.readFileSync(
-          `./src/images/entity/${inventoryItem.item.id}.png`,
-          'base64',
-        );
-        const dataURI = 'data:image/png;base64,' + itemImage;
-
-        const item: InventoryRow = { image: dataURI, quantity: inventoryItem.quantity };
-        content.inventorySlots.push(item);
-      }
-    }
-
-    const inventoryImage = await generateImage('./src/templates/inventory.html', content);
-
-    const attachment = new AttachmentBuilder(inventoryImage, { name: 'inventory.png' });
-    const inventoryEmbed = new EmbedBuilder()
-      .setColor('#0099ff')
-      .setImage('attachment://inventory.png');
-
-    interaction.reply({ embeds: [inventoryEmbed], files: [attachment] });
+    interaction.reply(await getInventory(filterInventory, user));
   },
 };
---
+++ src/util/getInventory.ts
@@ -0,0 +1,33 @@
+import { User } from 'src/entity/User';
+import { AttachedEmbed } from 'src/types/AttachedEmbed';
+import { InventoryContent, InventoryRow } from 'src/types/InventoryContent';
+import { generateImage } from './generateImage';
+import { AttachmentBuilder, EmbedBuilder } from 'discord.js';
+import { readFileSync } from 'fs';
+import { InventoryStack } from 'src/entity/InventoryStack';
+
+export async function getInventory(
+  filter: (arg: InventoryStack) => boolean,
+  user: User,
+): Promise<AttachedEmbed> {
+  const content: InventoryContent = { inventorySlots: [] };
+
+  for (const inventoryItem of user.inventory) {
+    if (filter(inventoryItem)) {
+      const itemImage = readFileSync(`./src/images/entity/${inventoryItem.item.id}.png`, 'base64');
+      const dataURI = 'data:image/png;base64,' + itemImage;
+
+      const item: InventoryRow = { image: dataURI, quantity: inventoryItem.quantity };
+      content.inventorySlots.push(item);
+    }
+  }
+
+  const inventoryImage = await generateImage('./src/templates/inventory.html', content);
+
+  const attachment = new AttachmentBuilder(inventoryImage, { name: 'inventory.png' });
+  const inventoryEmbed = new EmbedBuilder()
+    .setColor('#0099ff')
+    .setImage('attachment://inventory.png');
+
+  return { embeds: [inventoryEmbed], files: [attachment] };
+}
