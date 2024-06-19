534ccce436f3f42ffb5b21d2e273b81f4d609c59
metanoia02 <metanoia3902@gmail.com>
2024-06-19 20:21:53 +0100

    Fix commands interface

--- src/interfaces/ICommand.ts
+++ src/interfaces/ICommand.ts
@@ -1,11 +1,16 @@
-import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from '@discordjs/builders';
+import {
+  SlashCommandBuilder,
+  SlashCommandSubcommandsOnlyBuilder,
+  SlashCommandOptionsOnlyBuilder,
+} from '@discordjs/builders';
 import { ChatInputCommandInteraction, AutocompleteInteraction } from 'discord.js';
 
 export interface ICommand {
   data:
     | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>
     | SlashCommandBuilder
-    | SlashCommandSubcommandsOnlyBuilder;
+    | SlashCommandSubcommandsOnlyBuilder
+    | SlashCommandOptionsOnlyBuilder;
   execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
   autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>;
 }
